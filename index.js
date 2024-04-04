4d9d911459fbcf45420a7a2d20e416fa102a05c3
julydeath <manojkarajada.mk@gmail.com>
2024-04-04 10:01:37 +0530

    chore : init paystack webhook

---
+++ index.js
@@ -0,0 +1,38 @@
+// index.js file
+
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const crypto = require("crypto");
+const secret = process.env.SECRET_KEY;
+const bodyParser = require("body-parser");
+
+const port = 3001;
+
+// Paystack Webhook function starts here
+
+app.post("/webhook", function (req, res) {
+  //validate event
+  const hash = crypto
+    .createHmac("sha512", secret)
+    .update(JSON.stringify(req.body))
+    .digest("hex");
+
+  if (hash == req.headers["x-paystack-signature"]) {
+    // Retrieve the request's body
+    const event = req.body;
+    // Do something with event
+    if (event && event.event === "transfer.success") {
+      return res.status(200).json({ message: "Transfer successful" });
+    }
+  }
+
+  res.send(200);
+});
+
+// Paystack Webhook function ends here
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
